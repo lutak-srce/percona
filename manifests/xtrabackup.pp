0e6fdd57087b009aaeb362046a88da08e3396e4b
Jakov Sosic <jsosic@gmail.com>
2018-09-27 12:29:26 +0200

    Move percona xtrabackup to new RPM from yum repo

--- manifests/xtrabackup.pp
+++ manifests/xtrabackup.pp
@@ -1,15 +1,13 @@
-# Class: percona::xtrabackup
+#
+# = Class: percona::xtrabackup
 #
 # This module manages Percona MySQL xtrabackup
 #
 class percona::xtrabackup (
-  $package_ensure = '2.1.1-600.rhel6',
+  $package_name   = 'percona-xtrabackup-24',
 ){
-  require perl::mod::time::hires
-
   package { 'percona-xtrabackup' :
-    ensure   => $package_ensure,
-    provider => 'rpm',
-    source   => "http://www.percona.com/redir/downloads/XtraBackup/LATEST/RPM/rhel6/x86_64/percona-xtrabackup-${package_ensure}.x86_64.rpm",
+    ensure => present,
+    name   => $package_name,
   }
 }
